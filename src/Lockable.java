749e12423be0a4e9273e7f7809218f8d26349ba4
Michael George <georgem@wlwv.k12.or.us>
2020-01-16 09:17:16 -0800

    final commit

---
+++ src/Lockable.java
@@ -0,0 +1,11 @@
+public interface Lockable {
+
+  public void setKey (int key);
+
+  public void Locked (boolean isLocked);
+
+  public void Lockable();
+
+  //add lockable to BankAcct or Die class ??????????
+
+}
